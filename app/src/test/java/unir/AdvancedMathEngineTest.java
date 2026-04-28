7c96e2fde751e19ac52ae27dbee093193aa282a7
Victor Bocanegra <victor.bocanegra.e@gmail.com>
2026-04-27 19:44:36 -0600

    Test: Se añaden pruebas unitarias para la operación de raíz cuadrada (RED).
    - Se crea una clase de pruebas para validar el cálculo de raíces cuadradas.
    - Se agregan casos para números positivos, cero, uno y valores decimales.
    - Se valida la precisión esperada para la raíz cuadrada de 2.
    - Se comprueba que los números negativos lancen una excepción.
    - Se prepara la cobertura necesaria antes de completar la implementación de la funcionalidad.

---
+++ app/src/test/java/unir/AdvancedMathEngineTest.java
@@ -0,0 +1,66 @@
+package unir;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AdvancedMathEngineTest {
+    final MathEngine engine = new StandardMathEngine();
+    final double TOLERANCIA = 0.003;
+
+    // Pruebas unitarias correspondientes a la raíz cuadrada.
+    @Test
+    @DisplayName("La raíz cuadrada de un número positivo debe ser correcta.")
+    void testRaizCuadradaNumero() {
+        double resultado = engine.calcularRaiz(16);
+
+        assertEquals(4, resultado, "La raíz cuadrada de 16 debe ser 4");
+    }
+
+    @Test
+    @DisplayName("La raíz cuadrada de 1 es 1.")
+    void testRaizCuadradaUno() {
+        double resultado = engine.calcularRaiz(1);
+
+        assertEquals(1, resultado, "La raíz cuadrada de 1 debe ser 1");
+    }
+
+    @Test
+    @DisplayName("No se admiten números negativos por resultar en números imaginarios.")
+    void testRaizCuadradaNumeroNegativo() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            engine.calcularRaiz(-4);
+        }, "La raíz cuadrada de un número negativo debe lanzar IllegalArgumentException");
+
+    }
+
+    @Test
+    @DisplayName("La raíz cuadrada de 2 debe tener una precisión superior a 10^-3.")
+    void testRaizCuadradaDosPrecision() {
+        double resultado = engine.calcularRaiz(2);
+
+        assertEquals(1.414, resultado, 0.001, "La raíz de 2 debe ser aprox 1.414");
+    }
+
+    @Test
+    @DisplayName("La raíz cuadrada de un número entre 0 y 1.")
+    void testRaizCuadradaDecimal() {
+        double resultado = engine.calcularRaiz(0.25);
+
+        assertEquals(0.5, resultado, 0.001, "La raíz de 0.25 debe ser 0.5");
+    }
+
+
+    @Test
+    @DisplayName("La raíz cuadrada de 0 debe ser 0.")
+    void testRaizCuadradaCero() {
+        double resultado = engine.calcularRaiz(0);
+
+        assertEquals(0, resultado, 0.001, "La raíz cuadrada de 0 debe ser 0");
+    }
+
+
+
+}
